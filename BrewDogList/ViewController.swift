a1d69ba773e227706d3f04bfecb5efcce309be02
Igor Arsenkin <igor@arsenkin.name>
2020-01-30 09:38:57 +0200

    Initial Commit

---
+++ BrewDogList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrewDogList
+//
+//  Created by Ihor Arsenkin on 30.01.2020.
+//  Copyright © 2020 Igor Arsenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
