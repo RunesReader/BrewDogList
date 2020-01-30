a6eb54878112d334bd16164c07d1021018ba2aae
Igor Arsenkin <igor@arsenkin.name>
2020-01-30 11:33:49 +0200

    Create NetworkHandler.

---
+++ BrewDogList/Networking/NetworkHandler.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkHandler.swift
+//  BrewDogList
+//
+//  Created by Ihor Arsenkin on 30.01.2020.
+//  Copyright © 2020 Igor Arsenkin. All rights reserved.
+//
+
+import Foundation
+
+enum NetConfig {
+    static let post = "POST"
+    static let get = "GET"
+    static let baseURL = "https://api.punkapi.com/v2/"
+}
+
+enum Endpoint {
+    static let getBeers = NetConfig.baseURL + "beers"
+}
